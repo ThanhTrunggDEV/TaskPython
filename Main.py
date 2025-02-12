76e5565cd6e5cbac790af79518fbec3d62a3f3ee
Nguyễn Thành Trung <trungff07@gmail.com>
2025-02-13 00:12:40 +0700

    completed authentication and added some features

--- Main.py
+++ Main.py
@@ -1,15 +1,16 @@
-from email.policy import default
+
 from typing import Annotated
 from fastapi import FastAPI, Response,status
-from fastapi.params import Header, Depends
+from fastapi.params import Depends
 from fastapi.security import HTTPBasicCredentials, HTTPBasic
-from Model import User, Manage, UserInfo, UpdateInfo
+from Model import Manage, UserInfo, UpdateInfo
+
 
 app = FastAPI()
 security = HTTPBasic()
 USERNAME = "admin"
 PASSWORD = "test"
-manage = Manage([])
+manage = Manage()
 
 def authen(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
     if credentials.username == USERNAME and credentials.password == PASSWORD:
@@ -17,29 +18,36 @@ def authen(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
     return False
 
 @app.get("/get_user/{user_name}")
-async def get_user(user_name:str, Authen: Annotated[None, Depends(authen)]):
+async def get_user(user_name:str, Authen = Depends(authen)):
     if not Authen:
         return {"Message": "Access Denied"}
     for user in manage.list_users:
         if user.user_name == user_name:
             return user
-    return f"Not Found {user_name}"
 
-@app.delete("/del_user/{user_name}")
-async def del_user(user_name: str):
-   return manage.del_user(user_name)
+    return {"message" : f"Not Found {user_name}"}
+
+@app.delete("/del_user/{user_name}", status_code=status.HTTP_100_CONTINUE)
+async def del_user(user_name: str, Authen = Depends(authen)):
+    if not Authen:
+        return {"Message": "Access Denied"}
+    return manage.del_user(user_name)
 
 @app.get("/get_full_user/")
-async def get_full_user():
+async def get_full_user(Authen = Depends(authen)):
+    if not Authen:
+        return {"message": "Access Denied"}
     return manage.get_all_users()
 
 @app.post("/create_user/")
-async def create_us(user_info: UserInfo):
-    new_user = User(user_info.user_name, user_info.pass_word, user_info.phone_number, user_info.full_name)
-    manage.add_user(new_user)
-    return user_info
+async def create_user(user_info: UserInfo, Authen = Depends(authen)):
+    if not Authen:
+        return {"message": "Access Denied"}
+    return manage.add_user(user_info)
 
 @app.put("/update_user/{user_name}")
-async def update_user(user_name: str, new_info: UpdateInfo):
+async def update_user(user_name: str, new_info: UpdateInfo, Authen = Depends(authen)):
+    if not Authen:
+        return {"message": "Access Denied"}
     return manage.update_user(user_name, new_info)
 
--- Model.py
+++ Model.py
@@ -1,11 +1,14 @@
 from pydantic import BaseModel
 
-
 class UserInfo(BaseModel):
     user_name: str
     pass_word: str
     phone_number: str | None = None
     full_name: str
+    age: int
+class SearchInfo(BaseModel):
+    user_name: str | None = None
+    full_name: str | None = None
 
 class UpdateInfo(BaseModel):
     new_user_name: str | None = None
@@ -14,10 +17,11 @@ class UpdateInfo(BaseModel):
     new_full_name: str | None = None
 
 class User:
-    def __init__(self, user_name: str, pass_word: str, phone_number: str , full_name: str):
+    def __init__(self, user_name: str, pass_word: str, phone_number: str , full_name: str, age: int):
         self.user_name = user_name
         self.pass_word = pass_word
         self.full_name = full_name
+        self.age = age
         self.phone_number = phone_number
 
     def __str__(self):
@@ -26,32 +30,47 @@ class User:
         item.pass_word = self.pass_word
         item.phone_number = self.phone_number
         item.full_name = self.full_name
+        item.age = self.age
         return item
 
 class Manage:
-    def __init__(self, list_users: list):
-        self.list_users = list_users
-    def add_user(self, user: User):
-        self.list_users.append(user)
+    def __init__(self):
+        self.list_users = list()
+        self.existed = dict()
+    def add_user(self, user_info: UserInfo):
+        if self.existed.get(user_info.user_name):
+            return {"message": f"username {user_info.user_name} is existed"}
+        new_user = User(user_info.user_name, user_info.pass_word, user_info.phone_number, user_info.full_name, user_info.age)
+        self.list_users.append(new_user)
+        self.existed[new_user.user_name] = True
+        return {"message": f"added user {new_user.user_name}"}
     def del_user(self, user_name: str):
         for user in self.list_users:
             if user_name == user.user_name:
                 self.list_users.remove(user)
-                return True
-        return False
+                self.existed.pop(user_name)
+                return {"message": f"deleted user {user_name}"}
+        return {"message": f"Not Found user {user_name}"}
     def update_user(self, user_name, new_info: UpdateInfo):
-        index = 0
         for user in self.list_users:
             if user_name == user.user_name:
-                break
-            index = index + 1
-        if index >= len(self.list_users) == 0:
-            return {"message" : f"Not found user {user_name}"}
-
-        if new_info.new_user_name:
+                if new_info.new_user_name:
+                    user.user_name = new_info.new_user_name
+                    self.existed.pop(user_name)
+                    self.existed[new_info.new_user_name] = True
+                if new_info.new_pass_word:
+                    user.pass_word = new_info.new_pass_word
+                if new_info.new_full_name:
+                    user.full_name = new_info.new_full_name
+                if new_info.new_phone_number:
+                    user.phone_number = new_info.new_phone_number
+                return {"message": "Updated"}
+        return {"message": f"Not Found user {user_name}"}
 
     def get_all_users(self):
         result = list()
         for user in self.list_users:
             result.append(user)
         return result
+    def search(self):
+        pass
